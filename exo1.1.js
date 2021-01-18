436fedd0f6e16b53a7c40e524aab72190bfd59e2
root <fredgaloppin@gmail.com>
2021-01-18 16:43:10 +0100

    exo1.1

---
+++ exo1.1.js
@@ -0,0 +1,10 @@
+const readlineSync = require("readline-sync");
+
+	let firstName = readlineSync.question("What's your first name? ");
+	let name = readlineSync.question("What's your name? ");
+	let city = readlineSync.question("Wich city are you come from? ");
+	console.log("Your name is "+firstName +" "+ name +" and you live in "+city+".");
+	
+  
+  
+  
