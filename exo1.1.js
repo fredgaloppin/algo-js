314eb1c59d4ba6d9df18bd58fc92933328d881f8
root <fredgaloppin@gmail.com>
2021-01-18 16:47:54 +0100

    exo1.2

--- exo1.1.js
+++ exo1.1.js
@@ -1,9 +1,7 @@
 const readlineSync = require("readline-sync");
 
-	let firstName = readlineSync.question("What's your first name? ");
-	let name = readlineSync.question("What's your name? ");
-	let city = readlineSync.question("Wich city are you come from? ");
-	console.log("Your name is "+firstName +" "+ name +" and you live in "+city+".");
+	let name = "Dr. Livingston";
+	console.log(name + ", I presume.")
 	
   
   
