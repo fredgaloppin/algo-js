7f40ac0a381005dadf5db4b4941791f87256eeb0
root <fredgaloppin@gmail.com>
2021-01-18 16:27:57 +0100

    exo1.0.

---
+++ exo1.0.js
@@ -0,0 +1,31 @@
+const readlineSync = require("readline-sync");var carré = function (nombre) { return nombre * nombre };
+
+	var num1 = parseInt(readlineSync.question("Enter a first number "));
+	var num2 = parseInt(readlineSync.question("Enter a second number "));
+	var op = readlineSync.question("Enter an operator among +,-,*,/ ");
+	function calcul (num1,num2,op) {
+		switch(op){
+			case "+":
+			  result = num1 + num2;
+			  break;
+			case "-":
+			  result = num1 - num2;
+			  break;
+			case "*":
+			  result = num1 * num2;
+			  break;
+			case "/":
+			  result = num1 / num2;
+			  break;
+			default:
+			  result = "Calcul impossible"
+		  }
+		  return result;
+		}
+	var result = calcul(num1,num2,op); 
+	
+	console.log(num1+ " " + op + " " + num2 + " = " + result);
+	
+  
+  
+  
