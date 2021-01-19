d9fa246cdb73604e00982aae503ed9c1284569b4
root <fredgaloppin@gmail.com>
2021-01-19 12:32:52 +0100

    exo3.4

---
+++ exo3.4.js
@@ -0,0 +1,4 @@
+const readlineSync = require("readline-sync");
+
+let arrayOfNumbers = [4, 12, 62, 70, -10];
+console.log("max: "+(Math.max(...arrayOfNumbers))+ " & min: "+(Math.min(...arrayOfNumbers))); 
